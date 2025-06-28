c2ab1c91017c6142437c1e429a23ba7a0e699a23
godfri <godfri@kali>
2025-06-28 18:01:00 +0300

    Реалізовано клас Person та функцію create_person_list згідно з тестами

--- app/main.py
+++ app/main.py
@@ -1,8 +1,32 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person_list = []
+
+    for person_data in people:
+        name = person_data.get("name")
+        age = person_data.get("age")
+        if name is not None and age is not None:
+            person = Person(name, age)
+            person_list.append(person)
+
+    for person_data, person in zip(people, person_list):
+        if "wife" in person_data:
+            if person_data["wife"] is None:
+                continue
+            if person_data["wife"] in Person.people:
+                person.wife = Person.people[person_data["wife"]]
+        if "husband" in person_data:
+            if person_data["husband"] is None:
+                continue
+            if person_data["husband"] in Person.people:
+                person.husband = Person.people[person_data["husband"]]
+
+    return person_list
